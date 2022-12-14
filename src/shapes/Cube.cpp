98c100968a67788de1722b2adc022fefb7aef3b0
Darra Snow <darrasnow@The-Aeon-Engine.local>
2022-12-14 13:21:58 -0500

    cleaning up the file

--- src/shapes/Cube.cpp
+++ src/shapes/Cube.cpp
@@ -5,13 +5,6 @@ void Cube::updateParams(int param1) {
     m_param1 = param1;
     m_lookupSize = 1024;
 
-    makeCube();
-    setVertexData();
-}
-
-void Cube::makeCube() {
-    // Generate random vector lookup table
-
     m_randVecLookup.reserve(m_lookupSize);
 
     // Initialize random number generator
@@ -22,14 +15,14 @@ void Cube::makeCube() {
     {
       m_randVecLookup.push_back(glm::vec2(std::rand() * 2.0 / RAND_MAX - 1.0,
                                           std::rand() * 2.0 / RAND_MAX - 1.0));
-      }
+    }
+
+    makeFace();
 }
 
 // Helper for computePerlin() and, possibly, getColor()
 float Cube::interpolate(float A, float B, float alpha) {
-    // Task 4: implement your easing/interpolation function below
     float ease = 3 * pow(alpha, 2) - 2 * pow(alpha, 3);
-
     return A + ease * (B - A);
 }
 
@@ -42,7 +35,7 @@ glm::vec2 Cube::sampleRandomVector(int row, int col) {
 
 // Computes the intensity of Perlin noise at some point
 float Cube::computePerlin(float x, float y) {
-    // Task 1: get grid indices (as ints)
+    // Get grid indices (as ints)
     int X = (int)x;
     int Y = (int)y;
     glm::vec2 i1 = {X, Y};
@@ -50,20 +43,20 @@ float Cube::computePerlin(float x, float y) {
     glm::vec2 i3 = {X, Y + 1};
     glm::vec2 i4 = {X + 1, Y + 1};
 
-    // Task 2: compute offset vectors
+    // Compute offset vectors
     glm::vec2 input = {x,y};
     glm::vec2 off1 = input - i1;
     glm::vec2 off2 = input - i2;
     glm::vec2 off3 = input - i3;
     glm::vec2 off4 = input - i4;
 
-    // Task 3: compute the dot product between offset and grid vectors
+    // Compute the dot product between offset and grid vectors
     float dot1 = glm::dot(off1, sampleRandomVector(i1[0], i1[1]));
     float dot2 = glm::dot(off2, sampleRandomVector(i2[0], i2[1]));
     float dot3 = glm::dot(off3, sampleRandomVector(i3[0], i3[1]));
     float dot4 = glm::dot(off4, sampleRandomVector(i4[0], i4[1]));
 
-    // Task 5: use your interpolation function to produce the final value
+    // Use your interpolation function to produce the final value
     float inter1 = interpolate(dot1, dot2, x - i1[0]);
     float inter2 = interpolate(dot3, dot4, x - i1[0]);
 
@@ -75,17 +68,14 @@ float Cube::computePerlin(float x, float y) {
 // Returns a height value, z, by sampling a noise function
 float Cube::getHeight(float x, float y) {
 
-    // Task 6: modify this call to produce noise of a different frequency
-    //float z = computePerlin(x * 8, y * 8) / 8;
     float z = 0;
 
-    // Task 7: combine multiple different octaves of noise to produce fractal perlin noise
+    // Combine multiple different octaves of noise to produce fractal perlin noise
     z += computePerlin(x * 8, y * 8) / 8;
     z += computePerlin(x * 16, y * 16) / 16;
     z += computePerlin(x * 32, y * 32) / 32;
     z += computePerlin(x * 64, y * 64) / 64;
 
-    // Return 0 as placeholder
     return z;
 }
 
@@ -94,7 +84,6 @@ void Cube::makeTile(glm::vec3 topLeft,
                     glm::vec3 topRight,
                     glm::vec3 bottomLeft,
                     glm::vec3 bottomRight) {
-    // Task 2: create a tile (i.e. 2 triangles) based on 4 given points.
 
     //normals are destination - source
     glm::vec3 TLnormal = glm::normalize(glm::cross(bottomLeft - topLeft, topRight - topLeft));
@@ -124,13 +113,11 @@ void Cube::makeFace() {
     glm::vec3 topLeft, topRight, bottomLeft, bottomRight;
 
     float m_resolution = 5.0;
-    float m_halfRes = (m_resolution / 2.0);
-    float m_heightMultiplier = 2.0;
+    float m_halfRes = m_resolution / 2.0;
+    float m_heightMultiplier = 6.0;
 
-    float xVal;
-    float xValLong;
-    float yVal;
-    float yValLong;
+    float xVal, xValLong;
+    float yVal ,yValLong;
     float zVal;
 
 
@@ -162,14 +149,7 @@ void Cube::makeFace() {
 
 }
 
-
-void Cube::setVertexData() {
-
-    makeFace();
-}
-
 // Inserts a glm::vec3 into a vector of floats.
-// This will come in handy if you want to take advantage of vectors to build your shape!
 void Cube::insertVec3(std::vector<float> &data, glm::vec3 v) {
     data.push_back(v.x);
     data.push_back(v.y);
--- src/shapes/Cube.h
+++ src/shapes/Cube.h
@@ -8,24 +8,26 @@ class Cube
 public:
     void updateParams(int param1);
     std::vector<float> generateShape() { return m_vertexData; }
-    std::vector<glm::vec2> m_randVecLookup;
-    int m_lookupSize;
 
-    glm::vec2 sampleRandomVector(int row, int col);
-    float computePerlin(float x, float y);
-    float getHeight(float x, float y);
-    void makeCube();
-    float interpolate(float A, float B, float alpha);
 
 private:
+    std::vector<float> m_vertexData;
+    std::vector<glm::vec2> m_randVecLookup;
+
+    glm::vec2 sampleRandomVector(int row, int col);
+
+    int m_lookupSize;
+    int m_param1;
+
+    float computePerlin(float x, float y);
+    float getHeight(float x, float y);
+    float interpolate(float A, float B, float alpha);
+
     void insertVec3(std::vector<float> &data, glm::vec3 v);
-    void setVertexData();
     void makeTile(glm::vec3 topLeft,
                   glm::vec3 topRight,
                   glm::vec3 bottomLeft,
                   glm::vec3 bottomRight);
     void makeFace();
 
-    std::vector<float> m_vertexData;
-    int m_param1;
 };
